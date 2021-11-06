8e4936eb4d7e736940b9d5a8fcffb3b9861038c6
Amine-Frikha <aminfrikha@outlook.com>
2021-11-07 00:48:57 +0100

    Setup an express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+//Middleware that lets us accept json data into our API
+app.use(express.json());
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
