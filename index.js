277ee40b6566664de3b7eca478df1bb1e109f658
Amine-Frikha <aminfrikha@outlook.com>
2021-11-07 03:01:05 +0100

    Url shortener logic & routes

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ connectDB();
 //Middleware that lets us accept json data into our API
 app.use(express.json());
 
+// Define Routes
+app.use('/api/url', require('./routes/url'));
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
